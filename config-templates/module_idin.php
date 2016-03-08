ac567ee8ef9f402d90837af808c94aaac4eb3042
U-lamza-laptop\Lamza <ilie.halip@gmail.com>
2016-03-08 09:42:25 +0200

    added example config file for idin module

---
+++ config-templates/module_idin.php
@@ -0,0 +1,41 @@
+<?php
+/* 
+ * Configuration for the iDIN module.
+ */
+
+$config = array(
+    'merchantID' => '000000000',
+    'subID' => '0',
+    'returnUrl' => 'https://localhost/simplesaml/module.php/idin/return.php',
+    'directoryUrl' => 'https://my.bank/directory',
+    'transactionUrl' => 'https://my.bank/transaction',
+    'statusUrl' => 'https://my.bank/status',
+    
+    'merchant:certificate:file' => 'my-certificate.p12',
+    'merchant:certificate:password' => 'my-password',
+    'routingservice:certificate' => 'routing-service.cer',
+    'saml:certificate:file' => 'my-certificate.p12',
+    'saml:certificate:password' => 'my-password',
+    'servicelogs:enabled' => true,
+    'servicelogs:location' => 'C:\MyWebshop\idin\ServiceLogs',
+    'servicelogs:pattern' => '%Y-%M-%D\%h%m%s.%f-%a.xml',
+    
+    'store' => array(
+        'idin:Database',
+        'dsn' => 'mysql:host=localhost;dbname=simplesaml',
+        'username' => 'username',
+        'password' => 'password',
+        'table' => 'module.idin'
+    ),
+    
+    //'store' => array(
+    //    'idin:Memcache',
+    //    'servers' => array(
+    //        array(
+    //            'hostname' => 'localhost',
+    //            'port' => '11211'
+    //        ),
+    //    ),
+    //    'key' => 'simplesamlphp.module.idin.directory'
+    //),
+);
